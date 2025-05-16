c8e33e4934b74a1c507be1cb0564144a29e17a87
Bhuvnesh <bhuvnesh.bhuwan.golden@gmail.com>
2025-05-16 20:15:18 +0530

    Converting whole markdown file to a tree of html nodes

--- src/blockmarkdown.py
+++ src/blockmarkdown.py
@@ -1,4 +1,7 @@
 from enum import Enum
+from textnode import TextNode, TextType
+from htmlnode import HTMLNode, ParentNode, LeafNode
+from inlinemarkdown import InlineMarkdown
 
 class BlockType(Enum):
     PARAGRAPH = "Paragraph"
@@ -75,4 +78,203 @@ class BlockMarkDown():
                 return False 
             list_idx += 1
         return True 
+    
+
+    def markdown_to_html_node(self, markdown):
+        blocks = self.markdown_to_blocks(markdown)
+        root_node = ParentNode("div", [], None)
+
+        for block in blocks:
+            block_type = self.block_to_block_type(block)
+
+            if block_type == BlockType.PARAGRAPH:
+                p_node = self.get_paragraph_node(block)
+                root_node.children.append(p_node)
+            elif block_type == BlockType.HEADING:
+                h_node = self.get_heading_node(block)
+                root_node.children.append(h_node)
+            elif block_type == BlockType.QUOTE:
+                q_node = self.get_quote_node(block)
+                root_node.children.append(q_node)
+            elif block_type == BlockType.CODE:
+                c_node = self.get_code_node(block)
+                root_node.children.append(c_node)
+            elif block_type == BlockType.UNORDERED_LIST:
+                ul_node = self.get_unordered_list_node(block)
+                root_node.children.append(ul_node)
+            elif block_type == BlockType.ORDERED_LIST:
+                ol_node = self.get_ordered_list_node(block)
+                root_node.children.append(ol_node)
         
+        return root_node
+        
+    
+    def get_paragraph_node(self, block):
+        parts = block.split("\n")
+        block = " ".join(parts)
+
+        text_nodes = InlineMarkdown().text_to_textnodes(block)
+        if len(text_nodes) == 1:
+            return LeafNode("div", block, None)
+
+        p_node = ParentNode("p", [], None)
+        for text_node in text_nodes:
+            if text_node.text_type == TextType.TEXT:
+                p_node.children.append(LeafNode(None, text_node.text, None))
+            elif text_node.text_type == TextType.BOLD:
+                p_node.children.append(LeafNode("b", text_node.text, None))
+            elif text_node.text_type == TextType.CODE:
+                p_node.children.append(LeafNode("code", text_node.text, None))
+            elif text_node.text_type == TextType.ITALIC:
+                p_node.children.append(LeafNode("i", text_node.text, None))
+            elif text_node.text_type == TextType.IMAGE:
+                p_node.children.append(LeafNode("img", "", {"src" : text_node.url, "alt" : text_node.text}))
+            elif text_node.text_type == TextType.LINK:
+                p_node.children.append(LeafNode("a", text_node.text, {"href" : text_node.url}))
+        return p_node 
+    
+
+    def get_heading_node(self, block):
+        h_tag, h_text = self.get_heading_details(block)
+        parts = h_text.split("\n")
+        h_text = " ".join(parts)
+
+        text_nodes = InlineMarkdown().text_to_textnodes(h_text)
+        if len(text_nodes) == 1:
+            return LeafNode(h_tag, h_text, None)
+        
+        h_node = ParentNode(h_tag, [], None)
+        for text_node in text_nodes:
+            if text_node.text_type == TextType.TEXT:
+                h_node.children.append(LeafNode(None, text_node.text, None))
+            elif text_node.text_type == TextType.BOLD:
+                h_node.children.append(LeafNode("b", text_node.text, None))
+            elif text_node.text_type == TextType.CODE:
+                h_node.children.append(LeafNode("code", text_node.text, None))
+            elif text_node.text_type == TextType.ITALIC:
+                h_node.children.append(LeafNode("i", text_node.text, None))
+            elif text_node.text_type == TextType.IMAGE:
+                h_node.children.append(LeafNode("img", "", {"src" : text_node.url, "alt" : text_node.text}))
+            elif text_node.text_type == TextType.LINK:
+                h_node.children.append(LeafNode("a", text_node.text, {"href" : text_node.url}))
+        return h_node
+    
+
+    def get_quote_node(self, block):
+        q_tag, q_text = "blockquote", block[1:]
+        parts = q_text.split("\n")
+        q_text = " ".join(parts)
+
+        text_nodes = InlineMarkdown().text_to_textnodes(q_text)
+        if len(text_nodes) == 1:
+            return LeafNode(q_tag, q_text, None)
+        
+        q_node = ParentNode(q_tag, [], None)
+        for text_node in text_nodes:
+            if text_node.text_type == TextType.TEXT:
+                q_node.children.append(LeafNode(None, text_node.text, None))
+            elif text_node.text_type == TextType.BOLD:
+                q_node.children.append(LeafNode("b", text_node.text, None))
+            elif text_node.text_type == TextType.CODE:
+                q_node.children.append(LeafNode("code", text_node.text, None))
+            elif text_node.text_type == TextType.ITALIC:
+                q_node.children.append(LeafNode("i", text_node.text, None))
+            elif text_node.text_type == TextType.IMAGE:
+                q_node.children.append(LeafNode("img", "", {"src" : text_node.url, "alt" : text_node.text}))
+            elif text_node.text_type == TextType.LINK:
+                q_node.children.append(LeafNode("a", text_node.text, {"href" : text_node.url}))
+        return q_node
+    
+
+    def get_unordered_list_node(self, block):
+        ul_node = ParentNode("ul", [], None)
+        list_items = block.split("\n")
+
+        for list_item in list_items:
+            item_text = list_item[2:]
+            text_nodes = InlineMarkdown().text_to_textnodes(item_text)
+            if len(text_nodes) == 1:
+                ul_node.children.append(LeafNode("li", item_text, None))
+            
+            li_node = ParentNode("li", [], None)
+            for text_node in text_nodes:
+                if text_node.text_type == TextType.TEXT:
+                    li_node.children.append(LeafNode(None, text_node.text, None))
+                elif text_node.text_type == TextType.BOLD:
+                    li_node.children.append(LeafNode("b", text_node.text, None))
+                elif text_node.text_type == TextType.CODE:
+                    li_node.children.append(LeafNode("code", text_node.text, None))
+                elif text_node.text_type == TextType.ITALIC:
+                    li_node.children.append(LeafNode("i", text_node.text, None))
+                elif text_node.text_type == TextType.IMAGE:
+                    li_node.children.append(LeafNode("img", "", {"src" : text_node.url, "alt" : text_node.text}))
+                elif text_node.text_type == TextType.LINK:
+                    li_node.children.append(LeafNode("a", text_node.text, {"href" : text_node.url}))
+            ul_node.children.append(li_node)
+        
+        return ul_node
+    
+
+    def get_ordered_list_node(self, block):
+        ol_node = ParentNode("ol", [], None)
+        list_items = block.split("\n")
+
+        for list_item in list_items:
+            item_text = self.get_ordered_item_text(list_item)
+            text_nodes = InlineMarkdown().text_to_textnodes(item_text)
+            if len(text_nodes) == 1:
+                ol_node.children.append(LeafNode("li", item_text, None))
+            
+            li_node = ParentNode("li", [], None)
+            for text_node in text_nodes:
+                if text_node.text_type == TextType.TEXT:
+                    li_node.children.append(LeafNode(None, text_node.text, None))
+                elif text_node.text_type == TextType.BOLD:
+                    li_node.children.append(LeafNode("b", text_node.text, None))
+                elif text_node.text_type == TextType.CODE:
+                    li_node.children.append(LeafNode("code", text_node.text, None))
+                elif text_node.text_type == TextType.ITALIC:
+                    li_node.children.append(LeafNode("i", text_node.text, None))
+                elif text_node.text_type == TextType.IMAGE:
+                    li_node.children.append(LeafNode("img", "", {"src" : text_node.url, "alt" : text_node.text}))
+                elif text_node.text_type == TextType.LINK:
+                    li_node.children.append(LeafNode("a", text_node.text, {"href" : text_node.url}))
+            ol_node.children.append(li_node)
+        
+        return ol_node
+    
+
+    def get_code_node(self, block):
+        code_text = block[3:-3]
+        code_node = LeafNode("code", code_text, None)
+        pre_node = ParentNode("pre", [code_node], None)
+        return pre_node
+
+
+    def get_heading_details(self, block):
+        idx = 0
+        for c in block:
+            if c != "#":
+                break
+            idx += 1
+        
+        h_tag = f"h{idx}"
+        h_text = block[idx+1:]
+        return h_tag, h_text
+        
+
+    def get_ordered_item_text(self, block):
+        text = block.split('.', 1)[1].strip()
+        return text 
+        
+
+    
+
+
+
+
+
+
+    
+
+
--- src/main.py
+++ src/main.py
@@ -6,6 +6,41 @@ from blockmarkdown import BlockMarkDown
 
 def main():
     print("Welcome to Static Site Generator!")
+
+    md = """
+# This is heading 1
+
+This is a paragraph
+with a second line
+and with a third line
+
+## This is heading 2
+
+> This is a quote
+
+### This is heading 3
+
+```
+This is a code block
+with some code snippets
+```
+
+#### This is heading 4
+
+1. Ordered List Item 1
+2. Ordered List Item 2
+3. Ordered List Item 3
+
+##### This is heading 5
+
+- Unrdered List Item 1
+- Unordered List Item 2
+- Unordered List Item 3
+"""
+
+    node = BlockMarkDown().markdown_to_html_node(md)
+    html = node.to_html()
+    print(html)
     
     
 if __name__ == "__main__":
