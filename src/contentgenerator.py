dcc4586183470cfeeee7e6125e496794945457bb
Bhuvnesh <bhuvnesh.bhuwan.golden@gmail.com>
2025-06-26 18:37:50 +0530

    Changes to deploy the static site pages on Github Pages

--- src/contentgenerator.py
+++ src/contentgenerator.py
@@ -13,7 +13,7 @@ class ContentGenerator():
         raise ValueError("Heading not found in the markdown file")
     
     
-    def generate_page(self, src_file_path, template_path, dest_file_path):
+    def generate_page(self, src_file_path, template_path, dest_file_path, base_path):
         with open(src_file_path, 'r', encoding = 'utf-8') as file:
             markdown = file.read()
         
@@ -23,15 +23,18 @@ class ContentGenerator():
         with open(template_path, 'r', encoding = 'utf-8') as file:
             page_template = file.read()
         
-        page = page_template.replace("{{ Title }}", page_title).replace("{{ Content }}", page_content)
+        page = page_template.replace("{{ Title }}", page_title)
+        page = page.replace("{{ Content }}", page_content)
+        page = page.replace("href=\"/", f"href=\"{base_path}")
+        page = page.replace("src=\"/", f"src=\"{base_path}")
 
         with open(dest_file_path, 'w', encoding = 'utf-8') as file:
             file.write(page)
     
 
-    def generate_pages_recursive(self, src_path, template_path, dest_path):
+    def generate_pages_recursive(self, src_path, template_path, dest_path, base_path):
         if os.path.isfile(src_path):
-            self.generate_page(src_path, template_path, dest_path)
+            self.generate_page(src_path, template_path, dest_path, base_path)
             return 
         
         contents = os.listdir(src_path)
@@ -44,41 +47,10 @@ class ContentGenerator():
             else:
                 new_dest_path = os.path.join(dest_path, content.split('.')[0] + ".html")
 
-            self.generate_pages_recursive(new_src_path, template_path, new_dest_path)
+            self.generate_pages_recursive(new_src_path, template_path, new_dest_path, base_path)
     
 
-    def generate_content(self, src_path, template_path, dest_path):
+    def generate_content(self, src_path, template_path, dest_path, base_path):
         print(f"\nTransforming markdown files of directory: \'{src_path}\' to html files in directory: \'{dest_path}\'.")
-        self.generate_pages_recursive(src_path, template_path, dest_path)
-        print(f"Finished transforming. You can access all the files on http://localhost:8888/\n")
-
-
-
-
-
-
-        
-
-        
-
-        
-
-
-
-        
-        
-
-
-
-
-
-    
-
-
-        
-
-    
-
-        
-
-
+        self.generate_pages_recursive(src_path, template_path, dest_path, base_path)
+        print(f"Finished transforming. You can access all the files on docs directory\n")
--- src/main.py
+++ src/main.py
@@ -1,11 +1,17 @@
+import sys 
 from copystatictopublic import CopyStaticToPublic
 from contentgenerator import ContentGenerator
 
 
 def main():
     print("Welcome to Static Site Generator!")
-    CopyStaticToPublic().delete_and_copy("static", "public")
-    ContentGenerator().generate_content("content", "template.html", "public")
+    
+    base_path = "/"
+    if len(sys.argv) > 1:
+        base_path = sys.argv[1]
+        
+    CopyStaticToPublic().delete_and_copy("static", "docs")
+    ContentGenerator().generate_content("content", "template.html", "docs", base_path)
 
 
 if __name__ == "__main__":
